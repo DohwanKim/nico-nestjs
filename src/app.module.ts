8cb800b82cbcfad0a2f2870521ded2d6fe3fec2b
Dohwan <windboy098@gmail.com>
2023-12-21 18:01:54 +0900

    test: add e2e test(니코 강의 끝)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
 import { MoviesService } from './movies/movies.service';
+import { MovieController } from './movie/movie.controller';
 
 @Module({
   imports: [MoviesModule],
-  controllers: [AppController],
+  controllers: [AppController, MovieController],
   providers: [MoviesService],
 })
 export class AppModule {}
