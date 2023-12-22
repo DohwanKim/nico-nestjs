b9566858a88e06ec7b3ad1b65d3e69b2668e066c
Dohwan <windboy098@gmail.com>
2023-12-22 19:07:37 +0900

    feat: postgreSQL 연결 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'process';
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '../**/*.entity.{js,ts}'],
+      synchronize: true,
+    }),
+    MoviesModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
