ba0e4e9bf2b73c968087b7b586f26a4cf24b2641
Dohwan <windboy098@gmail.com>
2023-12-27 18:17:52 +0900

    refactor: 엔티티 클래스 이름 변경 반영

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
-import { Movie } from './entities/movie.entity';
+import { MovieEntity } from './entities/movie.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Movie])],
+  imports: [TypeOrmModule.forFeature([MovieEntity])],
   controllers: [MoviesController],
   providers: [MoviesService],
 })
